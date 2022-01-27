6c73703ab3929b9743e5b7611ae0cfa4b365e9bb
Austin Nguyen <austinnguyen@MacBook-Pro.local>
2022-01-27 12:04:38 -0600

    Review.js added

--- das_travel_frontend/src/components/Reviews.js
+++ das_travel_frontend/src/components/Reviews.js
@@ -4,12 +4,45 @@ import React, {Component} from 'react'
 export default class Reviews extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            review: ''
+          }
     }
    
+    handleSubmit = (event) => {
+        event.preventDefault()
+        // fetch to the backend
+        fetch(this.props.baseUrl + '/reviews', {
+          method: 'POST',
+          body: JSON.stringify({name: this.state.review}),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        }).then( res => {
+          return res.json()
+        }).then( data => {
+          //console.log(data)
+          this.props.addReview(data)
+          this.setState({
+            review: ''
+          })
+        })
+      }
+    
+      handleChange = (event) => {
+        // console.log(event.target.value)
+        this.setState({
+          review: event.target.value
+        })
+      }
+
     render(){
         return(
-           <div>
-           </div> 
+            <form onSubmit={this.handleSubmit}>
+            <label htmlFor="review">Review: </label>
+            <input type="text" id="review" name="review" onChange={ (e) => this.handleChange(e) } value={this.state.name}/>
+            <input type="submit" value="Add a review"/>
+          </form>
         )
     }
 }
